4772671778170654f90214a750179047a1b72c60
Aziz-Abdellaoui <85045660+Aziz-Abdellaoui@users.noreply.github.com>
2021-06-01 02:13:24 +0100

    Add files via upload

---
+++ Atelier 6/produits/src/main/java/com/Aziz/produits/entities/ProduitProjection.java
@@ -0,0 +1,8 @@
+package com.Aziz.produits.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "nomProd", types = { Produit.class })
+public interface ProduitProjection {
+	public String getNomProduit();
+}
